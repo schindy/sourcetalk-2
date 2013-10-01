bdd75d12cdcf113ae58724b789801462b0714b39
Axel Fontaine <business@axelfontaine.com>
2013-10-01 14:30:42 +0200

    Fixed deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-sshpass -p 'vagrant' ssh -q -p $1 -o StrictHostKeyChecking=no vagrant@10.0.2.2 <<'ENDSSH'
+PORT=$1
+VERSION=$2
+echo Deploying version $VERSION to 10.0.2.2:$PORT
+sshpass -p 'vagrant' ssh -q -p $PORT -o StrictHostKeyChecking=no vagrant@10.0.2.2 <<'ENDSSH'
 wget -q -O /dev/null http://localhost:33221/shutdown
 rm continuous-delivery-training-*.jar
-wget http://10.0.2.2:18081/artifactory/libs-release/sourcetalk-tage/continuous-delivery-training/$2/continuous-delivery-training-$2.jar
+wget http://10.0.2.2:18081/artifactory/libs-release/sourcetalk-tage/continuous-delivery-training/$VERSION/continuous-delivery-training-$VERSION.jar
 nohup java -jar continuous-delivery-training-$2.jar &
 ENDSSH
